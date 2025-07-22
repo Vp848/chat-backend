da2aa889ff59a5ed3af31338c376a43df4dca2cb
Vp848 <vikash7877sharma@gmail.com>
2025-07-22 21:20:07 +0530

    Added working index.js and .env

--- index.js
+++ index.js
@@ -53,5 +53,5 @@ io.on('connection', (socket) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(Server running on port ${PORT});
+    console.log(`Server running on port ${PORT}`);
 });
